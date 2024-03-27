2dc15f194579fb5f93f2c84d50024fa22a255d37
silvadrian <adrian.silvar7@gmail.com>
2024-03-27 14:04:46 -0300

    Fundamentos do .NET até o Curso Sintaxe e Tipos de Dados em C#

---
+++ fundamentos.NET/exemplofundamentos/Program.cs
@@ -0,0 +1,16 @@
+﻿using exemplofundamentos.Models;
+
+// PessoaFisica pf = new PessoaFisica();
+// pf.Nome = "Adrian";
+// pf.Idade = 23;
+// pf.Apresentar();
+
+// string _string = "TIPO STRING";
+// int _inteiro = 1;
+// double _altura = 10.8;
+// decimal _decimal = 1.8M;
+// bool _bool = true;
+
+// DateTime dataAtual = DateTime.Now;
+
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
